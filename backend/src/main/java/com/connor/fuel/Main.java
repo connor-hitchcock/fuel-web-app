385eee70eedaed25b5ba5d327653e6f2465116b2
Connor Hitchcock <connorhitchcock@Connors-MacBook-Pro.local>
2022-04-04 11:40:04 +1200

    Imported an Auth0 JWT library and setup methods for creating and decoding a JWT using the HCMA256 hashing algorithm. Having some issues with decoding though

--- backend/src/main/java/com/connor/fuel/Main.java
+++ backend/src/main/java/com/connor/fuel/Main.java
@@ -1,17 +1,32 @@
 package com.connor.fuel;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.connor.fuel.authentication.JWTHandler;
 import com.connor.fuel.data.CarDetailData;
 import com.connor.fuel.data.FuelPriceData;
 import com.connor.fuel.model.CarDetail;
 
+import java.util.HashMap;
+
 import static com.connor.fuel.controller.Logic.calcCostPer100km;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void printAllCarDetails() {
         for (CarDetail car: CarDetailData.allCarsDetails) {
             var costPer100km = calcCostPer100km(car, FuelPriceData.newZealand, 0.5f);
             System.out.printf("Fuel Cost: $%.2f " + car + "\n", costPer100km);
         }
+    }
+
+    public static void main(String[] args) {
+        JWTHandler jwtHandler = new JWTHandler();
+        var payload = new HashMap<String, String>();
+        payload.put("msg", "Hello World");
+        String token = jwtHandler.createSignJWT(payload);
+        System.out.printf("Created Token: %s\n", token);
+        DecodedJWT verifiedToken = jwtHandler.verifyJWT(token);
+        System.out.printf("Verified Token: %s\n", verifiedToken.getPayload());
+        DecodedJWT decodedToken = jwtHandler.decodeJWT(token);
+        System.out.printf("Decoded Token: %s\n", decodedToken.getPayload());
     }
 }
---
+++ backend/src/main/java/com/connor/fuel/authentication/JWTHandler.java
@@ -0,0 +1,80 @@
+package com.connor.fuel.authentication;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Map;
+
+//note: The secret key must have a bit size >= the size of the output hash.
+//note: using HMAC256 secret key >= 256 bits.
+public class JWTHandler {
+    private final String secret = "FED5A77F61E99149B25F6DDAABB21";
+
+    /**
+     * Creates and signs the JWT token
+     * @param payload the payload of the JWT
+     * @return signed JWT token
+     */
+    public String createSignJWT(Map<String, String> payload) {
+        String token = null;
+        try {
+            Algorithm algorithmHS = Algorithm.HMAC256(secret);
+            token = JWT.create()
+                    .withIssuer("auth0")
+                    .withPayload(payload)
+                    .sign(algorithmHS);
+        } catch (JWTCreationException e) {
+            //Invalid Signing configuration / Couldn't convert claims.
+            System.out.println(e);
+        }
+       return token;
+    }
+
+    /**
+     * Verify a JWT has been signed using the secret
+     * @param token the JWT in question
+     * @return the decoded JWT
+     */
+    public DecodedJWT verifyJWT(String token) {
+        DecodedJWT jwt = null;
+        try {
+            Algorithm algorithmHS = Algorithm.HMAC256(secret);
+            JWTVerifier verifier = JWT.require(algorithmHS)
+                    .withIssuer("auth0")
+                    .build(); //Reusable verifier instance
+            jwt = verifier.verify(token);
+        } catch (JWTVerificationException e) {
+            //Invalid signature/claims
+            System.out.println(e);
+        }
+        return jwt;
+    }
+
+    /**
+     * Decode a JWT
+     * @param token the JWT in question
+     * @return the decoded JWT
+     */
+    public DecodedJWT decodeJWT(String token) {
+        DecodedJWT jwt = null;
+        try {
+            jwt = JWT.decode(token);
+        } catch (JWTDecodeException e) {
+            //Invalid token
+            System.out.println(e);
+        }
+        return jwt;
+    }
+
+
+
+
+
+
+}
